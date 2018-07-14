08adc12020b62bd286ca9de756405ddf3bcbb671
Unknown <ralphnorman91@gmail.com>
2018-07-14 09:10:42 +0100

    Initial Command Pattern

---
+++ Assets/Scripts/CommandPattern/Invoker.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// Invoker class used to tell the Command to carry out the request
+/// </summary>
+namespace DesignPatterns.Structural.Command
+{
+    public class Invoker
+    {
+        private Command _command;
+
+        public void SetCommand(Command command)
+        {
+            _command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            _command.Execute();
+        }
+    }
+}
